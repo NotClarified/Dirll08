e72ae1ccff77478f6d2462d10ddd1c5295b1210f
JeonDae <james911256@gmail.com>
2023-10-05 10:39:27 +0900

    class Grass 생성

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,7 +1,14 @@
 from pico2d import *
 
 # Game object class here
+class Grass:
+    def __init__(self): #self 자세한 설명 생략, 생성이된 객체를 가리키는 변수
+        self.image = load_image('grass.png')
 
+    def draw(self):
+        self.image.draw(400,30)
+
+    def update(self): pass
 
 
 def handle_events():
@@ -12,19 +19,22 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-
-open_canvas()
 def reset_world():
     global running
+    global grass
     running = True
+    grass = Grass() # grass 객체 변수, Grass() 클래스
 
 def update_world():
+    grass.update()
     pass
 
 def render_world():
     clear_canvas()
+    grass.draw()
     update_canvas()
 
+open_canvas()
 # initialization code
 reset_world()
 
